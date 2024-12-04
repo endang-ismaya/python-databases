f92c2f52ea2f8bf7f43915a315bb16a20573fae9
endangismaya <endang.ismaya@gmail.com>
2024-12-04 14:06:51 +0700

    SQLAlchemy info

--- SQLAlchemy_info.py
+++ SQLAlchemy_info.py
@@ -5,4 +5,13 @@
 # SQLAlchemy ORM
     It encapsulates the schema with business objects. This makes your database interactions
     feel more like normal Python Code
+
+    Advantages:
+    - Interact with the database using the language in Pythonic way
+    - Speeds up development process
+
+    Disadvantages:
+    - In SQL, more performant queries can be written
+    - ORM shifts database complexity into application code
+    - ORM Prevents you to understand what SQL is doing under the hood
 """
