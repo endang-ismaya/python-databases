780d854979296481b75275dcba1299a894bc558d
endangismaya <endang.ismaya@gmail.com>
2024-12-04 14:01:30 +0700

    first commit setup

---
+++ SQLAlchemy_info.py
@@ -0,0 +1,8 @@
+"""
+# SQLAlchemy Core
+    Schema-centric usage which has focused around tables, keys, and SQL Concepts
+
+# SQLAlchemy ORM
+    It encapsulates the schema with business objects. This makes your database interactions
+    feel more like normal Python Code
+"""
