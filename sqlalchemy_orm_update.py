d34cdd06420d764be308c66422bd1b4c14f25ec7
endangismaya <endang.ismaya@gmail.com>
2024-12-05 11:17:17 +0700

    add sqlalchemy_orm update

---
+++ sqlalchemy_orm_update.py
@@ -0,0 +1,33 @@
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.orm import declarative_base, sessionmaker
+
+engine = create_engine("sqlite:///databases/hr_orm.db")
+Base = declarative_base()
+
+
+class Employees(Base):
+    __tablename__ = "employees"
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False)
+    position = Column(String, nullable=False)
+
+
+# Create tables
+Base.metadata.create_all(engine)
+
+# Create a session
+Session = sessionmaker(bind=engine)
+session = Session()
+
+query = session.get(Employees, 2)
+
+if query:
+    query.name = "Gracie"
+    query.position = "Agile Instructor"
+    print(query.name, query.position)
+
+    # commit changes
+    session.commit()
+
+# Close the session
+session.close()
